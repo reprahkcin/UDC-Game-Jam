2b70abffde5fb279361271330b445d52b35d2831
Nick Harper <bignickharper@gmail.com>
2021-09-05 20:19:08 -0700

    #19 #20  Shovel

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,66 +4,75 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // -------------------------------------------------
-    // Player Components
-    // --------------------------------------------------
-    // Get Collider
-    public Collider2D collider;
+  // -------------------------------------------------
+  // Player Components
+  // --------------------------------------------------
+  // Get Collider
+  public Collider2D collider;
 
-    // Get Rigidbody
-    public Rigidbody2D rb;
+  // Get Rigidbody
+  public Rigidbody2D rb;
 
-    // Get Animator component
-    public Animator animator;
+  // Get Animator component
+  public Animator animator;
 
-    // Float for moveSpeed
-    public float moveSpeed = 5f;
+  // Get Weapon Animator component
+  // TODO: Add this in the inspector
+  public Animator weaponAnimator;
 
-    // -------------------------------------------------
-    // Assorted Variables
-    // --------------------------------------------------
-    // Vector2 for movement
-    Vector2 movement;
+  // Float for moveSpeed
+  public float moveSpeed = 5f;
 
-    // -------------------------------------------------
-    // Methods
-    // --------------------------------------------------
-    void Start()
+  // -------------------------------------------------
+  // Assorted Variables
+  // --------------------------------------------------
+  // Vector2 for movement
+  Vector2 movement;
+
+  // -------------------------------------------------
+  // Methods
+  // --------------------------------------------------
+  void Start()
+  {
+    // get the Collider2D component
+    collider = GetComponent<Collider2D>();
+
+    // get the Rigidbody2D component
+    rb = GetComponent<Rigidbody2D>();
+
+    // get the Animator component
+    animator = GetComponent<Animator>();
+  }
+
+  void Update()
+  {
+    // get the horizontal and vertical axis
+    float x = Input.GetAxis("Horizontal");
+    float y = Input.GetAxis("Vertical");
+
+    // Create the movement vector and normalize it
+    movement = new Vector2(x, y).normalized;
+
+    // Set horizontal animator parameter to horizontal movement
+    animator.SetFloat("Horizontal", movement.x);
+
+    // Set vertical animator parameter to vertical movement
+    animator.SetFloat("Vertical", movement.y);
+
+    // Set speed animator parameter to movement magnitude
+    animator.SetFloat("Speed", movement.magnitude);
+
+    // If space is pressed
+    if (Input.GetKeyDown(KeyCode.Space))
     {
-        // get the Collider2D component
-        collider = GetComponent<Collider2D>();
-
-        // get the Rigidbody2D component
-        rb = GetComponent<Rigidbody2D>();
-
-        // get the Animator component
-        animator = GetComponent<Animator>();
+      // Set trigger for "Sword_Attack_Right"
+      weaponAnimator.SetTrigger("Sword_Attack_Right");
     }
+  }
 
-    void Update()
-    {
-        // get the horizontal and vertical axis
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-
-        // Create the movement vector and normalize it
-        movement = new Vector2(x, y).normalized;
-
-        // Set horizontal animator parameter to horizontal movement
-        animator.SetFloat("Horizontal", movement.x);
-
-        // Set vertical animator parameter to vertical movement
-        animator.SetFloat("Vertical", movement.y);
-
-        // Set speed animator parameter to movement magnitude
-        animator.SetFloat("Speed", movement.magnitude);
-    }
-
-    void FixedUpdate()
-    {
-        // Move the player
-        rb
-            .MovePosition(rb.position +
-            movement * moveSpeed * Time.fixedDeltaTime);
-    }
+  void FixedUpdate()
+  {
+    // Move the player
+    rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+  }
 }
